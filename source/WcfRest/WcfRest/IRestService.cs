d1fd7995fad7266703f009aac3e9e2d026deccba
HenriqueKeppel <franzonikeppel@yahoo.com.br>
2019-04-04 00:26:48 -0300

    Enfileiramento com sucesso

---
+++ source/WcfRest/WcfRest/IRestService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfRest
+{
+    // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da interface "IRestService" no arquivo de código e configuração ao mesmo tempo.
+    [ServiceContract]
+    public interface IRestService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+                 ResponseFormat = WebMessageFormat.Json,
+                 RequestFormat = WebMessageFormat.Json,
+                 BodyStyle = WebMessageBodyStyle.Wrapped,
+                 UriTemplate = "EnfileiraDados/{dados}")]
+        string Enfileira(string dados);
+    }
+}
---
+++ source/WcfRest/WcfRest/RestService.svc.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfRest
+{
+    // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da classe "RestService" no arquivo de código, svc e configuração ao mesmo tempo.
+    // OBSERVAÇÃO: Para iniciar o cliente de teste do WCF para testar esse serviço, selecione RestService.svc ou RestService.svc.cs no Gerenciador de Soluções e inicie a depuração.
+    public class RestService : IRestService
+    {
+        public string Enfileira(string dados)
+        {
+            try
+            {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare(queue: "messageQueue",
+                                             durable: false,
+                                             exclusive: false,
+                                             autoDelete: false,
+                                             arguments: null);
+
+                        var body = Encoding.UTF8.GetBytes(dados);
+
+                        channel.BasicPublish(exchange: "",
+                                             routingKey: "messageQueue",
+                                             basicProperties: null,
+                                             body: body);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return "BadRequest: " + ex.Message;
+            }
+            return "OK";
+        }
+    }
+}
